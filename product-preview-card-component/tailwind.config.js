f77865a6245f151407481cb2bc17acf9f580e927
Vivek Alhat <vivekalhat14@outlook.com>
2023-07-08 21:13:24 +0530

    add product preview card component

---
+++ product-preview-card-component/tailwind.config.js
@@ -0,0 +1,28 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        cream: "hsl(30, 38%, 92%)",
+        "dark-cyan": "hsl(158, 36%, 37%)",
+        "very-dark-blue": "hsl(212, 21%, 14%)",
+        "dark-grayish-blue": "hsl(228, 12%, 48%)",
+      },
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        fraunces: ["Fraunces", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        desktop:
+          "url('/product-preview-card-component/images/image-product-desktop.jpg')",
+        mobile:
+          "url('/product-preview-card-component/images/image-product-mobile.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
