fdef48f32b384020377bb5c929ee3aad9c4e7ece
Vivek Alhat <vivekalhat14@outlook.com>
2023-07-15 22:44:30 +0530

    add responsive width and height

--- advice-generator-app/src/components/Loader.jsx
+++ advice-generator-app/src/components/Loader.jsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div role="status">
+    <div role="status" className="w-[300px] grid place-items-center">
       <svg
         aria-hidden="true"
         className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-light-cyan"
