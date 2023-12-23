47891f890b965e4cb55f50f145732c16e7a268c2
Vivek Alhat <vivekalhat14@outlook.com>
2023-12-24 02:50:28 +0530

    add accessibility changes

--- faq-accordion/src/App.jsx
+++ faq-accordion/src/App.jsx
@@ -1,31 +1,61 @@
+import { useState } from "react";
 import faq from "../data/data.json";
 
 function App() {
+  const [activeId, setActiveId] = useState(1);
+
   return (
     <main className="h-screen bg-light-pink relative">
       <div className="bg-mobile sm:bg-desktop h-60"></div>
-      <section className="bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col gap-8 rounded-xl lg:p-8 shadow-md">
-        <div className="inline-flex items-center gap-4">
-          <img src="/assets/images/icon-star.svg" alt="star icon" />
-          <h1 className="font-bold text-5xl text-dark-purple">FAQs</h1>
+      <section className="bg-white absolute max-w-min min-w-[350px] md:min-w-[600px] lg:min-w-[700px] left-1/2 transform -translate-x-1/2 flex flex-col gap-8 rounded-xl p-4 lg:p-8 shadow-lg -my-28">
+        <div className="flex items-center gap-4">
+          <img
+            src="/assets/images/icon-star.svg"
+            alt="star icon"
+            className="h-5 sm:h-full"
+          />
+          <h1 className="font-bold text-3xl sm:text-5xl text-dark-purple">
+            FAQs
+          </h1>
         </div>
         <div className="flex flex-col gap-4">
           {faq.map((f) => (
-            <div key={f.id} className="space-y-3">
-              <div className="inline-flex items-center justify-between w-full gap-8">
-                <p className="text-dark-purple font-semibold">{f.question}</p>
+            <div
+              key={f.id}
+              className={`space-y-3 py-3 ${
+                f.id !== 4 && "border-b border-light-pink"
+              }`}
+            >
+              <button
+                onClick={() => setActiveId((id) => (id === f.id ? 0 : f.id))}
+                aria-expanded={activeId === f.id}
+                aria-controls="accordion-content"
+                type="button"
+                className="flex items-center justify-between w-full gap-8 focus:p-3"
+              >
+                <p className="text-dark-purple text-left hover:text-purple cursor-pointer font-semibold">
+                  {f.question}
+                </p>
+
                 <img
-                  src="/assets/images/icon-plus.svg"
-                  alt="expand"
+                  src={`${
+                    activeId === f.id
+                      ? "/assets/images/icon-minus.svg"
+                      : "/assets/images/icon-plus.svg"
+                  }`}
+                  alt={`${activeId === f.id ? "collapse" : "expand"}`}
                   className="cursor-pointer"
+                  onClick={() => setActiveId((id) => (id === f.id ? 0 : f.id))}
                 />
-                {/* <img
-                  src="/assets/images/icon-minus.svg"
-                  alt="collpase"
-                  className="cursor-pointer"
-                /> */}
-              </div>
-              {/* <p>{f.answer}</p> */}
+              </button>
+              <p
+                id="accordion-content"
+                className="text-grayish-purple"
+                hidden={activeId !== f.id}
+                role="region"
+              >
+                {f.answer}
+              </p>
             </div>
           ))}
         </div>
