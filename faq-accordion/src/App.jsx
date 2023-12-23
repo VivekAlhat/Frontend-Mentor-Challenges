e2c55fe7f664a0a8f7d8614eed395a54ae3cfcd1
Vivek Alhat <vivekalhat14@outlook.com>
2023-12-23 22:43:18 +0530

    create base design

--- faq-accordion/src/App.jsx
+++ faq-accordion/src/App.jsx
@@ -1,5 +1,37 @@
+import faq from "../data/data.json";
+
 function App() {
-  return null;
+  return (
+    <main className="h-screen bg-light-pink relative">
+      <div className="bg-mobile sm:bg-desktop h-60"></div>
+      <section className="bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col gap-8 rounded-xl lg:p-8 shadow-md">
+        <div className="inline-flex items-center gap-4">
+          <img src="/assets/images/icon-star.svg" alt="star icon" />
+          <h1 className="font-bold text-5xl text-dark-purple">FAQs</h1>
+        </div>
+        <div className="flex flex-col gap-4">
+          {faq.map((f) => (
+            <div key={f.id} className="space-y-3">
+              <div className="inline-flex items-center justify-between w-full gap-8">
+                <p className="text-dark-purple font-semibold">{f.question}</p>
+                <img
+                  src="/assets/images/icon-plus.svg"
+                  alt="expand"
+                  className="cursor-pointer"
+                />
+                {/* <img
+                  src="/assets/images/icon-minus.svg"
+                  alt="collpase"
+                  className="cursor-pointer"
+                /> */}
+              </div>
+              {/* <p>{f.answer}</p> */}
+            </div>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- faq-accordion/tailwind.config.js
+++ faq-accordion/tailwind.config.js
@@ -1,8 +1,24 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        "light-pink": "hsl(275, 100%, 97%)",
+        "grayish-purple": "hsl(292, 16%, 49%)",
+        "dark-purple": "hsl(292, 42%, 14%)",
+      },
+      fontFamily: {
+        sans: ["Work Sans", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        desktop: "url(/assets/images/background-pattern-desktop.svg)",
+        mobile: "url(/assets/images/background-pattern-mobile.svg)",
+      },
+    },
   },
   plugins: [],
 };
