794174d97af5196f023794ceb2ca34b8ebe33714
Vivek Alhat <vivekalhat14@outlook.com>
2024-01-21 20:58:18 +0530

    clear region on back navigation

--- rest-countries/components/back.tsx
+++ rest-countries/components/back.tsx
@@ -1,14 +1,26 @@
+"use client";
+
+import { useRegion } from "@/providers/region-provider";
+
 import { ArrowLeft } from "lucide-react";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Back() {
+  const { setRegion } = useRegion();
+  const router = useRouter();
+
+  const redirectToHome = () => {
+    setRegion("");
+    router.push("/");
+  };
+
   return (
-    <Link
-      href="/"
+    <div
       className="flex items-center gap-3 w-fit py-2 px-6 dark:bg-dark-mode-el shadow-custom rounded-md hover:bg-slate-50 dark:hover:bg-slate-700"
+      onClick={redirectToHome}
     >
       <ArrowLeft className="h-5 w-5" />
       <p>Back</p>
-    </Link>
+    </div>
   );
 }
