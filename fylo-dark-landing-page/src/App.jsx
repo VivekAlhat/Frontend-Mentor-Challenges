a090db696c00e5efe16dd24a09afc06d889199ff
Vivek Alhat <vivekalhat14@outlook.com>
2024-02-10 23:32:20 +0530

    add background curve

--- fylo-dark-landing-page/src/App.jsx
+++ fylo-dark-landing-page/src/App.jsx
@@ -4,25 +4,39 @@ function App() {
   return (
     <main className="text-white">
       <Header />
-      <section className="bg-dark-blue-intro-signup px-6 md:px-8 lg:px-12 pt-12 pb-28 flex flex-col items-center justify-center gap-8">
-        <img
-          src="/illustration-intro.png"
-          alt="Illustration Intro"
-          className="h-fit w-[500px]"
-        />
-        <div className="font-opensans text-center space-y-6 max-w-xl">
-          <h1 className="text-3xl font-bold leading-[3rem] lg:leading-[3rem] md:text-3xl">
-            All your files in one secure location, accessible anywhere.
-          </h1>
-          <p className="text-lg text-white/80 leading-[1.5rem]">
-            Fylo stores all your most important files in one secure location.
-            Access them wherever you need, share and collaborate with friends
-            family, and co-workers.
-          </p>
-          <button className="font-semibold font-raleway border-none outline-none py-3 w-[250px] bg-gradient-to-r from-cyang to-blueg rounded-full">
-            Get Started
-          </button>
+      <section className="bg-dark-blue-intro-signup flex flex-col items-center justify-center gap-8 relative">
+        <div className="z-10 max-w-xl px-6 md:px-8 lg:px-12 pt-12 pb-28 md:mb-24">
+          <img
+            src="/illustration-intro.png"
+            alt="Illustration Intro"
+            className="h-fit w-[600px]"
+          />
+          <div className="font-opensans text-center space-y-8">
+            <h1 className="text-3xl font-bold leading-[3rem] lg:leading-[3rem] md:text-3xl">
+              All your files in one secure location, accessible anywhere.
+            </h1>
+            <p className="text-lg text-white/80 leading-[1.5rem]">
+              Fylo stores all your most important files in one secure location.
+              Access them wherever you need, share and collaborate with friends
+              family, and co-workers.
+            </p>
+            <button className="font-semibold font-raleway border-none outline-none py-3 w-[250px] bg-gradient-to-r from-cyang to-blueg rounded-full">
+              Get Started
+            </button>
+          </div>
         </div>
+        <picture>
+          <source
+            srcSet="/bg-curvy-desktop.svg"
+            media="(min-width: 600px)"
+            className="w-full absolute bottom-0 left-0"
+          />
+          <img
+            src="/bg-curvy-mobile.svg"
+            alt="curve"
+            className="w-full absolute bottom-0 left-0"
+          />
+        </picture>
       </section>
       <section className="bg-dark-blue-main grid grid-cols-1 md:grid-cols-2 gap-8 p-6 md:p-12 lg:p-24 place-items-center">
         <div className="flex flex-col items-center justify-center text-center max-w-md gap-4 p-4">
--- fylo-dark-landing-page/src/components/Header.jsx
+++ fylo-dark-landing-page/src/components/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
-    <nav className="flex items-center justify-between bg-dark-blue-intro-signup pt-8 px-6 md:px-8 lg:px-12">
+    <nav className="flex flex-col md:flex-row gap-6 items-center justify-between bg-dark-blue-intro-signup pt-8 px-6 md:px-8 lg:px-12">
       <img src="/logo.svg" alt="Fylo Logo" className="h-fit w-24" />
-      <ul className="font-raleway flex items-center justify-between gap-5 md:gap-10">
+      <ul className="font-raleway flex items-center justify-between gap-4 md:gap-10">
         <li>Features</li>
         <li>Team</li>
         <li>Sign In</li>
